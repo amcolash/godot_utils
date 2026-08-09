f8b1ed6fdb3c692758d31046b2be271017da3bb3
Andrew McOlash <amcolash@gmail.com>
2026-08-09 00:20:05 -0700

    fix bug in debug controller when input actions are not defined

--- debug/debug_controller.gd
+++ debug/debug_controller.gd
@@ -17,23 +17,24 @@ func _input(event: InputEvent) -> void:
   if !OS.is_debug_build():
     return
 
-  if event.is_action("close"):
+  if InputMap.has_action("close") and event.is_action("close"):
     get_tree().quit()
 
-  if event.is_action_pressed("speed", true):
-    Engine.time_scale = 20.0
-    Engine.physics_ticks_per_second = int(60 * Engine.time_scale)
-    Engine.max_physics_steps_per_frame = 100
+  if InputMap.has_action("speed"):
+    if event.is_action_pressed("speed", true):
+      Engine.time_scale = 20.0
+      Engine.physics_ticks_per_second = int(60 * Engine.time_scale)
+      Engine.max_physics_steps_per_frame = 100
 
-  if event.is_action_released("speed"):
-    Engine.time_scale = 1.0
-    Engine.physics_ticks_per_second = 60
-    Engine.max_physics_steps_per_frame = 8
+    if event.is_action_released("speed"):
+      Engine.time_scale = 1.0
+      Engine.physics_ticks_per_second = 60
+      Engine.max_physics_steps_per_frame = 8
 
-  if event.is_action_pressed("restart"):
+  if InputMap.has_action("restart") and event.is_action_pressed("restart"):
     get_tree().reload_current_scene()
 
   # Handle custom actions
   for action in custom_actions.keys():
-    if event.is_action_pressed(action):
+    if InputMap.has_action(action) and event.is_action_pressed(action):
       custom_actions[action].call()
